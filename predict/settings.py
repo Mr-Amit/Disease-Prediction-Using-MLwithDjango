d4b5e626c450149e74b14e464b8ed570bf75bdfa
Mr-Amit <amit.kumar221099@gmail.com>
2022-01-21 23:47:52 +0530

    tNe

--- predict/settings.py
+++ predict/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s&+i4(no$u)zz4)ai#=&7c=uh@4td-u6(s1u$(87z=rk)amnc7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['ml-predictor-app.herokuapp.com', '127.0.0.1']
 
